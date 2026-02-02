1f04f13463545fe1d8cbc8b8b816e0e6fdb98a75
paulich07 <50242426+paulich07@users.noreply.github.com>
2026-02-02 23:47:00 +0100

    go

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 23:44:21 by plichota          #+#    #+#             */
+/*   Updated: 2026/02/02 23:46:48 by plichota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_HPP
+# define SCALAR_CONVERTER_HPP
+
+class ScalarConverter
+{
+    public:
+};
+
+#endif
